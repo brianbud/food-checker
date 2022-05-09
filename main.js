cef724ebd8bb4f84787074f5f5ec08aad0e10030
Brian Bud <brian@brianbud.com>
2022-05-08 23:05:13 -0400

    add eventListener for button

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function getFetch(userInput) {
-    const choice = userInput
-    const url = `https://world.openfoodfacts.org/api/v0/product/${choice}.json`
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch() {
+    let inputVal = document.getElementById('barcode').value
+    const url = `https://world.openfoodfacts.org/api/v0/product/${inputVal}.json`
 
     fetch(url)
         .then(response => response.json())
         .then(data => console.log(data))
-        .catch(err => console.log('error ${err}'));
+        .catch(err => console.log(`error ${err}`));
 }
