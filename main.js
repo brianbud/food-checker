511a1798ab00fa3c5ffcfad45ad9b6e3e5dfb7a2
Brian Bud <brian@brianbud.com>
2022-05-09 23:27:55 -0400

    limit barcode input to 12 digits

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ document.querySelector('button').addEventListener('click', getFetch)
 
 function getFetch() {
     let inputVal = document.getElementById('barcode').value
+
+    if (inputVal.length !== 12) {
+        alert(`Barcode needs to be 12 characters`)
+        return;
+    }
     const url = `https://world.openfoodfacts.org/api/v0/product/${inputVal}.json`
 
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            if (data.status === 1) {
+
+            } else if (data.status === 0) {
+                alert(`Product ${inputVal} not found.
+                Please try another.`)
+            }
+        })
         .catch(err => console.log(`error ${err}`));
 }
