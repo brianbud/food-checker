5dba419fb486dcc099ce71709b78244c8e37b692
Brian Bud <brian@brianbud.com>
2022-05-30 22:36:16 -0400

    delete exisiting table when searching new items

--- main.js
+++ main.js
@@ -40,6 +40,10 @@ class ProductInfo {
     listIngredients(){
         let tableRef = document.getElementById('ingredient-table')
 
+        for(let i = 1; i< tableRef.rows.length;){
+            tableRef.deleteRow(i);
+        }
+
         for(let key in this.ingredients){
             let newRow = tableRef.insertRow(-1)
             let newICell = newRow.insertCell(0)
